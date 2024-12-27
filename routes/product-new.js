d787ae36f2981ffa6a7f1a408fcc811d9520e867
Ihor-Omelchenko <ihor.omelchenko.official@gmail.com>
2024-12-27 21:07:29 +0200

    test: new route

--- routes/product-new.js
+++ routes/product-new.js
@@ -5,12 +5,14 @@ const router = express.Router();
 
 router.get('/', async (req, res) => {
     console.log('Request received at /product-new');
-    try {
-        const product = await Product.find();
-        res.status(200).json(product);
-    } catch (err) {
-        res.status(500).json({ message: 'Error fetching product', error: err });
-    }
+    // try {
+    //     const product = await Product.find();
+    //     res.status(200).json(product);
+    // } catch (err) {
+    //     res.status(500).json({ message: 'Error fetching product', error: err });
+    // }
+
+    res.json({ message: 'This is the /product-new route!' });
 });
 
 module.exports = router;
