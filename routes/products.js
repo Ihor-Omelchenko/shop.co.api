5a6b3d5688011033e597d8902ac7c90678e1c677
Ihor-Omelchenko <ihor.omelchenko.official@gmail.com>
2025-01-27 12:50:47 +0200

    add: filter

--- routes/products.js
+++ routes/products.js
@@ -4,26 +4,41 @@ const router = express.Router();
 
 router.get('/', async (req, res) => {
     try {
-        const page = parseInt(req.query.page) || 1;
-        const limit = parseInt(req.query.limit) || 10;
+        const { page = 1, limit = 10, ...filters } = req.query;
+
+        const query = {};
+
+        if (filters.id) {
+            query._id = filters.id;
+        }
+        if (filters.name) {
+            query.name = { $regex: filters.name, $options: 'i' };
+        }
+        if (filters.category) {
+            query.category = filters.category;
+        }
+        if (filters.minPrice && filters.maxPrice) {
+            query.price = { $gte: filters.minPrice, $lte: filters.maxPrice };
+        }
 
         const skip = (page - 1) * limit;
 
-        const products = await Product.find()
+        const products = await Product.find(query)
             .skip(skip)
-            .limit(limit);
+            .limit(parseInt(limit));
 
-        const totalRecords = await Product.countDocuments();
+        const totalRecords = await Product.countDocuments(query);
 
         res.status(200).json({
             products,
             totalRecords,
-            currentPage: page,
-            limit: Number(req.query.limit),
+            currentPage: parseInt(page),
+            limit: parseInt(limit),
             totalPages: Math.ceil(totalRecords / limit),
         });
     } catch (err) {
-        res.status(500).json({error: 'Error fetching product'});
+        console.error(err);
+        res.status(500).json({ error: 'Error fetching products' });
     }
 });
 
